57f06074412395773e0289d17446f028e2423141
megha-r20 <megha202005@gmail.com>
2025-02-26 13:42:06 +0530

    done

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/menu', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
